674c6e2f1f0058ed238d3d1d587002f2b06f2ec7
Hao Nguyen <hao.ict56@gmail.com>
2025-10-27 15:59:09 +0900

    update rocknix-joypad: fix key duplication issue at open

--- package/rocknix-joypad/rocknix-joypad.mk
+++ package/rocknix-joypad/rocknix-joypad.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ROCKNIX_JOYPAD_VERSION = 43245fbce81f28f56e9174e5a8cd94c72c97b161
-ROCKNIX_JOYPAD_SITE = https://github.com/ROCKNIX/rocknix-joypad
+ROCKNIX_JOYPAD_VERSION = b4f05d047244ea755cbdc050742ee2d6c68fe21d
+ROCKNIX_JOYPAD_SITE = https://github.com/haoict/tinilinux-joypad.git
 ROCKNIX_JOYPAD_SITE_METHOD = git
 ROCKNIX_JOYPAD_LICENSE = GPL
 
