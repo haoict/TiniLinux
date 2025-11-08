58ab45ad4fde8bff67fe3b4d6e263b3f44ecc160
Hao Nguyen <hao.ict56@gmail.com>
2025-11-08 20:17:35 +0900

    update simple-terminal.mk

--- package/simple-terminal/simple-terminal.mk
+++ package/simple-terminal/simple-terminal.mk
@@ -4,16 +4,19 @@
 #
 ################################################################################
 
-SIMPLE_TERMINAL_VERSION = 1.0
+SIMPLE_TERMINAL_VERSION = 2.0.0
 SIMPLE_TERMINAL_SITE = $(BR2_EXTERNAL_TiniLinux_PATH)/package/simple-terminal
 SIMPLE_TERMINAL_SITE_METHOD = local
-SIMPLE_TERMINAL_DEPENDENCIES = sdl2
+SIMPLE_TERMINAL_DEPENDENCIES = sdl2 sdl2_ttf
 
 # $(info BR2_DEFCONFIG: $(BR2_DEFCONFIG))
+SIMPLE_TERMINAL_MAKE_OPTS = VERSION=$(SIMPLE_TERMINAL_VERSION)
 ifeq ($(findstring h700,$(BR2_DEFCONFIG)),h700)
-	SIMPLE_TERMINAL_MAKE_OPTS = PLATFORM=buildroot_h700
+	SIMPLE_TERMINAL_MAKE_OPTS += PLATFORM=h700
 else ifeq ($(findstring rgb30,$(BR2_DEFCONFIG)),rgb30)
-	SIMPLE_TERMINAL_MAKE_OPTS = PLATFORM=buildroot_rgb30
+	SIMPLE_TERMINAL_MAKE_OPTS += PLATFORM=rgb30
+else ifeq ($(findstring raspberrypi,$(BR2_DEFCONFIG)),raspberrypi)
+	SIMPLE_TERMINAL_MAKE_OPTS += PLATFORM=raspberrypi
 endif
 
 define SIMPLE_TERMINAL_BUILD_CMDS
