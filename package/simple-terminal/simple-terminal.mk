65972a6e20c02b651599b27ca6fe8e33291762e5
Hao Nguyen <hao.ict56@gmail.com>
2025-11-08 14:47:25 +0900

    simple-teminal sdl2 migration
    * init migration
    
    * fix tab, ctrl keys, remove unneeded devices
    
    * apply clang-format
    
    * update screen init
    
    * remove unused code
    
    * remove unused code 2
    
    * add back scale_to_size
    
    * refactor makefile
    
    * separate tty logic
    
    * add ttf font render
    
    * add scale option
    
    * refine options
    
    * improve vt100
    
    * add toolchain and license
    
    * rename some argv
    
    * format files with c/c++ vs code extension
    
    * refactor sdlinit
    
    * refactor xw variable name
    
    * refactor customkeys
    
    * change credit title
    
    * refactor more
    
    * add screenshot event
    
    * rename some variables
    
    * add Mov osk button
    
    * show popup message when take screenshot
    
    * support keyboard with ttf
    
    * add more support for h700
    
    * fix bug for rgb30
    
    * update simple-launcher commands
    
    * remove sdl12-compat package

--- package/simple-terminal/simple-terminal.mk
+++ package/simple-terminal/simple-terminal.mk
@@ -7,15 +7,14 @@
 SIMPLE_TERMINAL_VERSION = 1.0
 SIMPLE_TERMINAL_SITE = $(BR2_EXTERNAL_TiniLinux_PATH)/package/simple-terminal
 SIMPLE_TERMINAL_SITE_METHOD = local
-SIMPLE_TERMINAL_DEPENDENCIES = sdl12-compat
+SIMPLE_TERMINAL_DEPENDENCIES = sdl2
 
 # $(info BR2_DEFCONFIG: $(BR2_DEFCONFIG))
 ifeq ($(findstring h700,$(BR2_DEFCONFIG)),h700)
-	SIMPLE_TERMINAL_MAKE_OPTS = UNION_PLATFORM=buildroot_h700
+	SIMPLE_TERMINAL_MAKE_OPTS = PLATFORM=buildroot_h700
 else ifeq ($(findstring rgb30,$(BR2_DEFCONFIG)),rgb30)
-	SIMPLE_TERMINAL_MAKE_OPTS = UNION_PLATFORM=buildroot_rgb30
+	SIMPLE_TERMINAL_MAKE_OPTS = PLATFORM=buildroot_rgb30
 endif
-# $(info SIMPLE_TERMINAL_MAKE_OPTS: $(SIMPLE_TERMINAL_MAKE_OPTS))
 
 define SIMPLE_TERMINAL_BUILD_CMDS
     $(MAKE) $(SIMPLE_TERMINAL_MAKE_OPTS) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
@@ -23,7 +22,7 @@ endef
 
 define SIMPLE_TERMINAL_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/local/bin
-    $(INSTALL) -D -m 0755 $(@D)/build/SimpleTerminal  $(TARGET_DIR)/usr/local/bin
+    $(INSTALL) -D -m 0755 $(@D)/simple-terminal  $(TARGET_DIR)/usr/local/bin
 endef
 
 $(eval $(generic-package))
