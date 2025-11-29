e0c51e204ff358c3c6f6ea3fb811b8096a649c4f
Hao Nguyen <hao.ict56@gmail.com>
2025-11-29 12:52:36 +0900

    update h700

--- package/simple-terminal/simple-terminal.mk
+++ package/simple-terminal/simple-terminal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SIMPLE_TERMINAL_VERSION = 2.0.0
+SIMPLE_TERMINAL_VERSION = 0d85d27
 SIMPLE_TERMINAL_SITE = https://github.com/haoict/SimpleTerminal.git
 SIMPLE_TERMINAL_SITE_METHOD = git
 SIMPLE_TERMINAL_DEPENDENCIES = sdl2 sdl2_ttf
