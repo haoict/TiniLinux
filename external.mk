c92e46391b076262749ffece529198507c04e99a
Hao Nguyen <hao.ict56@gmail.com>
2025-12-16 18:27:57 +0900

    add make run-qemu command

--- external.mk
+++ external.mk
@@ -2,18 +2,29 @@ include $(sort $(wildcard $(BR2_EXTERNAL_TiniLinux_PATH)/package/*/*.mk))
 
 img:
 	cd $(BR2_EXTERNAL_TiniLinux_PATH); \
-	BOARD=$(BR2_DEFCONFIG); \
 	if [ -f $(BINARIES_DIR)/rootfs.squashfs ]; then \
-		BOARD=$$(basename $$BOARD _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-squashfs-rootless.sh; \
+		BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-squashfs-rootless.sh; \
 	else \
-		BOARD=$$(basename $$BOARD _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-rootless.sh; \
+		BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-rootless.sh; \
 	fi
-
 
 flash:
 	cd $(BR2_EXTERNAL_TiniLinux_PATH); \
-	BOARD=$(BR2_DEFCONFIG); \
-	BOARD=$$(basename $$BOARD _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/flash-to-sdcard.sh
+	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/flash-to-sdcard.sh
 
 clean-target:
 	rm -rf $(BASE_TARGET_DIR) && find $(BUILD_DIR) -name ".stamp_target_installed" -delete && rm -f $(BUILD_DIR)/host-gcc-final-*/.stamp_host_installed
+
+run-qemu:
+	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig); \
+	ARCH=$${BOARD#*_}; ARCH=$${ARCH#*_}; ARCH=$${ARCH%%_*}; \
+	echo -e "\n\nStarting QEMU for board $$BOARD | arch $$ARCH ..."; \
+	cd $(CONFIG_DIR); \
+	./host/bin/qemu-system-$$ARCH -M virt -cpu cortex-a53 -nographic -smp 1 \
+	  -kernel images/Image \
+	  -initrd images/initramfs \
+	  -append "rootwait squashfspart=/dev/vda1 squashfsroot=rootfs.squashfs overlayfs=/dev/vda2 console=ttyAMA0" \
+	  -netdev user,id=eth0 \
+	  -device virtio-net-device,netdev=eth0 \
+	  -drive file=images/tinilinux-$$BOARD.img,if=none,format=raw,id=hd0 \
+	  -device virtio-blk-device,drive=hd0
