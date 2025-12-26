af928c819d74240d6e3ebce10574e0141f6f84a3
Hao Nguyen <hao.ict56@gmail.com>
2025-12-26 17:06:01 +0900

    migrate initramfs package to kconfig-package type but menuconfig is not working yet

--- package/initramfs/initramfs.mk
+++ package/initramfs/initramfs.mk
@@ -8,6 +8,10 @@ INITRAMFS_VERSION = 1.0.0
 INITRAMFS_SITE = https://www.busybox.net/downloads
 INITRAMFS_SOURCE = busybox-1.37.0.tar.bz2
 
+INITRAMFS_MAKE_ENV = \
+	$(TARGET_MAKE_ENV) \
+	CFLAGS="$(TARGET_CFLAGS)"
+
 INITRAMFS_MAKE_OPTS = \
 	AR="$(TARGET_AR)" \
 	NM="$(TARGET_NM)" \
@@ -17,16 +21,18 @@ INITRAMFS_MAKE_OPTS = \
 	EXTRA_LDFLAGS="$(TARGET_LDFLAGS)" \
 	CROSS_COMPILE="$(TARGET_CROSS)" \
 
+INITRAMFS_KCONFIG_FILE = $(BR2_EXTERNAL_TiniLinux_PATH)/package/initramfs/busybox.conf
+INITRAMFS_KCONFIG_EDITORS = menuconfig
+INITRAMFS_KCONFIG_OPTS = $(INITRAMFS_MAKE_OPTS) HOSTCC="$(HOSTCC_NOCCACHE)"
+
 define INITRAMFS_BUILD_CMDS
-	# copy custom busybox config from package dir to build dir
-	cp $(BR2_EXTERNAL_TiniLinux_PATH)/package/initramfs/busybox.conf $(@D)/.config
-	$(TARGET_MAKE_ENV) CFLAGS="$(TARGET_CFLAGS)" $(MAKE) $(INITRAMFS_MAKE_OPTS) -C $(@D)
+	$(INITRAMFS_MAKE_ENV) $(MAKE) $(INITRAMFS_MAKE_OPTS) -C $(@D)
 endef
 
 define INITRAMFS_INSTALL_TARGET_CMDS
 	# run busybox make install
 	rm -rf $(@D)/initramfs
-	$(TARGET_MAKE_ENV) $(MAKE) $(INITRAMFS_MAKE_OPTS) CONFIG_PREFIX=$(@D)/initramfs -C $(@D) install
+	$(INITRAMFS_MAKE_ENV) $(MAKE) $(INITRAMFS_MAKE_OPTS) CONFIG_PREFIX=$(@D)/initramfs -C $(@D) install
 
 	# prepare filesystem
 	mkdir -p $(@D)/initramfs/{dev,proc,sys,run,tmp,newroot}
@@ -37,4 +43,4 @@ define INITRAMFS_INSTALL_TARGET_CMDS
 	cd $(@D)/initramfs && find . | cpio -o -H newc | gzip > $(BINARIES_DIR)/initramfs
 endef
 
-$(eval $(generic-package))
+$(eval $(kconfig-package))
