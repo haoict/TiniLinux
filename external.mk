d4a8c9227af3508559a72a0366112573534e2b32
Hao Nguyen <hao.ict56@gmail.com>
2025-12-28 20:04:47 +0900

    rename boot cmdline squashfspart->bootpart, squashfsroot->squashfsimg

--- external.mk
+++ external.mk
@@ -1,16 +1,18 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_TiniLinux_PATH)/package/*/*.mk))
 
+BOARD=$(shell basename $(BR2_DEFCONFIG) _defconfig)
+
 img:
 	cd $(BR2_EXTERNAL_TiniLinux_PATH); \
 	if [ -f $(BINARIES_DIR)/rootfs.tar ]; then \
-		BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-rootrw-rootless.sh; \
+		BOARD=$(BOARD) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-rootrw-rootless.sh; \
 	else \
-		BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-squashfs-rootless.sh; \
+		BOARD=$(BOARD) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/mk-flashable-img-squashfs-rootless.sh; \
 	fi
 
 flash:
 	cd $(BR2_EXTERNAL_TiniLinux_PATH); \
-	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/flash-to-sdcard.sh
+	BOARD=$(BOARD) $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/flash-to-sdcard.sh
 
 cleantarget:
 	rm -rf $(BASE_TARGET_DIR) && find $(BUILD_DIR) -name ".stamp_target_installed" -delete && rm -f $(BUILD_DIR)/host-gcc-final-*/.stamp_host_installed
@@ -19,32 +21,26 @@ savefconf:
 	cd $(CONFIG_DIR) && $(BR2_EXTERNAL_TiniLinux_PATH)/board/common/save-fragment-defconfig.sh
 
 runqemu:
-	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig); \
-	ARCH=$${BOARD#*_}; ARCH=$${ARCH#*_}; ARCH=$${ARCH%%_*}; \
-	echo -e "\n\nStarting QEMU for board $$BOARD | arch $$ARCH ..."; \
-	cd $(CONFIG_DIR); \
-	qemu-system-$$ARCH -M virt -cpu cortex-a53 -smp 1 -m 1G \
-		-kernel images/Image \
-		-initrd images/initramfs \
-		-append "rootwait squashfspart=/dev/vda1 squashfsroot=rootfs.squashfs overlayfs=/dev/vda2 console=ttyAMA0" \
+	cd $(BINARIES_DIR); \
+	qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 1 -m 1G \
+		-kernel Image \
+		-initrd initramfs \
+		-append "rootwait bootpart=/dev/vda1 squashfsimg=rootfs.squashfs overlayfs=/dev/vda2 console=ttyAMA0" \
 		-netdev user,id=eth0 \
 		-device virtio-net-device,netdev=eth0 \
-		-drive file=images/tinilinux-$$BOARD.img,if=none,format=raw,id=hd0 \
+		-drive file=tinilinux-$(BOARD).img,if=none,format=raw,id=hd0 \
 		-device virtio-blk-device,drive=hd0 \
 		-nographic
 
 runqemugui:
-	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig); \
-	ARCH=$${BOARD#*_}; ARCH=$${ARCH#*_}; ARCH=$${ARCH%%_*}; \
-	echo -e "\n\nStarting QEMU for board $$BOARD | arch $$ARCH ..."; \
-	cd $(CONFIG_DIR); \
-	qemu-system-$$ARCH -M virt -cpu cortex-a53 -smp 2 -m 1G \
-		-kernel images/Image \
-		-initrd images/initramfs \
-		-append "rootwait squashfspart=/dev/vda1 squashfsroot=rootfs.squashfs overlayfs=/dev/vda2 console=ttyAMA0" \
+	cd $(BINARIES_DIR); \
+	qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 2 -m 1G \
+		-kernel Image \
+		-initrd initramfs \
+		-append "rootwait bootpart=/dev/vda1 squashfsimg=rootfs.squashfs overlayfs=/dev/vda2 console=ttyAMA0" \
 		-netdev user,id=eth0 \
 		-device virtio-net-device,netdev=eth0 \
-		-drive file=images/tinilinux-$$BOARD.img,if=none,format=raw,id=hd0 \
+		-drive file=tinilinux-$(BOARD).img,if=none,format=raw,id=hd0 \
 		-device virtio-blk-device,drive=hd0 \
 		-device virtio-gpu-gl-pci,xres=640,yres=480 -display gtk,gl=on \
 		-device virtio-keyboard-pci \
@@ -53,18 +49,14 @@ runqemugui:
 
 # unused
 runqemux64:
-	BOARD=$$(basename $(BR2_DEFCONFIG) _defconfig); \
-	ARCH=$${BOARD#*_}; ARCH=$${ARCH#*_}; ARCH=$${ARCH%%_*}; \
-	if [ "$$ARCH" = "x64" ]; then ARCH="x86_64"; fi; \
-	echo -e "\n\nStarting QEMU for board $$BOARD | arch $$ARCH ..."; \
-	cd $(CONFIG_DIR); \
-	qemu-system-$$ARCH -M q35 -smp 1 -m 1G \
-		-kernel images/bzImage \
-		-initrd images/initramfs \
-		-append "rootwait squashfspart=/dev/vda1 squashfsroot=rootfs.squashfs overlayfs=/dev/vda2 console=ttyS0" \
+	cd $(BINARIES_DIR); \
+	qemu-system-x86_64 -M q35 -smp 1 -m 1G \
+		-kernel bzImage \
+		-initrd initramfs \
+		-append "rootwait bootpart=/dev/vda1 squashfsimg=rootfs.squashfs overlayfs=/dev/vda2 console=ttyS0" \
 		-netdev user,id=eth0 \
 		-device virtio-net-pci,netdev=eth0 \
-		-drive file=images/tinilinux-$$BOARD.img,if=none,format=raw,id=hd0 \
+		-drive file=tinilinux-$(BOARD).img,if=none,format=raw,id=hd0 \
 		-device virtio-blk-pci,drive=hd0 \
 		-nographic \
 		-serial mon:stdio
