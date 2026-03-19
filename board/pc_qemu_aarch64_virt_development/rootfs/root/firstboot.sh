bbdb75c1d0d84849f616ab11938ba0a411643ddf
Hao Nguyen <hao.ict56@gmail.com>
2026-03-19 23:55:58 +0900

    create users by users_tables, fix sudo binary

--- board/pc_qemu_aarch64_virt_development/rootfs/root/firstboot.sh
+++ board/pc_qemu_aarch64_virt_development/rootfs/root/firstboot.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-touch /root/.vimrc
-mv /root/firstboot.sh /root/.firstboot-done.sh
 
+# Fix vim
+touch /root/.vimrc
+
+# Fix users
+chmod u+s /usr/bin/sudo
+chmod u+s /usr/bin/passwd
+chown -R admin:tinilinux /home/admin
+chown -R viewer:tinilinux /home/viewer
+
+# Fix DNS
 resolvectl dns eth0 8.8.8.8 8.8.4.4
+
+# Done
+mv /root/firstboot.sh /root/.firstboot-done.sh
 
 # FIXME: Workaround for docker/podman with /var directory as it always fails on firstboot. We need to reboot once to make it work.
 echo "Rebooting to complete first boot setup..." >/dev/tty0
