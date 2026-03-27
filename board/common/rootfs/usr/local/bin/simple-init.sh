c916eef3b1e3b1f0a97402572286ca48c095f236
Hao Nguyen <hao.ict56@gmail.com>
2026-03-27 23:39:25 +0900

    refactor simple-init service: separate launcher from simple-init

--- board/common/rootfs/usr/local/bin/simple-init.sh
+++ board/common/rootfs/usr/local/bin/simple-init.sh
@@ -1,1 +1,7 @@
-# Placeholder
+#!/bin/sh
+
+# This file is meant to be overwritten by specific board simple-init.sh
+
+if [ -f /root/firstboot.sh ]; then
+    /root/firstboot.sh
+fi
--- board/pc_qemu_aarch64_virt/rootfs/usr/local/bin/simple-init.sh
+++ board/pc_qemu_aarch64_virt/rootfs/usr/local/bin/simple-init.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-if [ -f /root/firstboot.sh ]; then
-    /root/firstboot.sh
-    systemctl set-default graphical.target
-    sleep 2
-    systemctl enable sway.service --now
-    sleep 2
-    systemctl enable launcher.service --now
-fi
+[ -f /root/firstboot.sh ] && /root/firstboot.sh
 
 echo 3 > /proc/sys/kernel/printk
-
-# cd /usr/local/bin && /usr/local/bin/simple-launcher &
-
-sleep infinity
