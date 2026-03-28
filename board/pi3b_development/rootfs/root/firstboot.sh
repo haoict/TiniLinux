0cbb21bbab2c33a40a73596a9c14c597b81e39ae
Hao Nguyen <hao.ict56@gmail.com>
2026-03-29 00:02:12 +0900

    refactor development files

--- board/pi3b_development/rootfs/root/firstboot.sh
+++ board/pi3b_development/rootfs/root/firstboot.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-chown -R admin:tinilinux ${TARGET_DIR}/home/admin
-chown -R viewer:tinilinux ${TARGET_DIR}/home/viewer
-
-sleep 3
-systemctl disable --now bluetooth
+systemctl disable bluetooth
+systemctl enable dockerd.service
 
 # Done
 mv /root/firstboot.sh /root/.firstboot-done.sh
@@ -12,5 +9,4 @@ mv /root/firstboot.sh /root/.firstboot-done.sh
 # FIXME: Workaround for docker/podman with /var directory as it always fails on firstboot. We need to reboot once to make it work.
 echo "Rebooting to complete first boot setup..." >/dev/tty0
 sleep 5
-systemctl enable dockerd.service
 systemctl reboot
