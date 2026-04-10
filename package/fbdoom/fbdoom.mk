e13f015caf6d8e204672116848c214886e3f4c85
Hao Nguyen <hao.ict56@gmail.com>
2026-04-11 04:00:09 +0900

    add fbdoom package

---
+++ package/fbdoom/fbdoom.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# fbdoom package
+#
+################################################################################
+
+FBDOOM_VERSION = e33fcbb5ff5c96c358eac57a3695ebf33d8e6f62
+FBDOOM_SITE = https://github.com/haoict/fbDOOM.git
+FBDOOM_SITE_METHOD = git
+
+define FBDOOM_BUILD_CMDS
+    $(MAKE) CROSS_COMPILE=$(TARGET_CROSS) CC=$(TARGET_CC) LD="$(TARGET_LD)" NOSDL=1 -C $(@D)/fbdoom
+endef
+
+define FBDOOM_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/local/bin
+    $(INSTALL) -D -m 0755 $(@D)/fbdoom/fbdoom  $(TARGET_DIR)/usr/local/bin
+
+    mkdir -p $(TARGET_DIR)/usr/share/games/doom/
+    wget -O- https://github.com/nneonneo/universal-doom/raw/refs/heads/main/DOOM1.WAD > $(TARGET_DIR)/usr/share/games/doom/DOOM1.WAD
+endef
+
+$(eval $(generic-package))
