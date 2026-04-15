67a137f05344d9f7d629f26f88f84200d312b379
Hao Nguyen <hao.ict56@gmail.com>
2026-04-15 12:32:32 +0900

    update docker package

--- package/docker-custom/containerization.mk
+++ package/docker-custom/containerization.mk
@@ -1,26 +1,24 @@
 ################################################################################
 #
-# docker
+# docker-custom
 #
 ################################################################################
 
-CONTAINERIZATION_VERSION = 1.0.0
-CONTAINERIZATION_SITE = $(BR2_EXTERNAL_TiniLinux_PATH)/package/containerization
-CONTAINERIZATION_SITE_METHOD = local
+DOCKER_CUSTOM_VERSION = 1.0.0
+DOCKER_CUSTOM_SITE = $(BR2_EXTERNAL_TiniLinux_PATH)/package/docker-custom
+DOCKER_CUSTOM_SITE_METHOD = local
 
-define CONTAINERIZATION_BUILD_CMDS
+define DOCKER_CUSTOM_BUILD_CMDS
 	cd $(@D); \
 	wget https://download.docker.com/linux/static/stable/aarch64/docker-29.3.0.tgz; \
 	tar xzvf docker-29.3.0.tgz; \
 	wget https://github.com/docker/compose/releases/download/v5.1.0/docker-compose-linux-aarch64;
 endef
 
-define CONTAINERIZATION_INSTALL_TARGET_CMDS
+define DOCKER_CUSTOM_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/docker/*  $(TARGET_DIR)/usr/bin/
 	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/
-	mkdir -p ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/
-	$(INSTALL) -D -m 0644 $(@D)/dockerd.service  $(TARGET_DIR)/usr/lib/systemd/system
-	#ln -sf /usr/lib/systemd/system/dockerd.service ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/dockerd.service
+	$(INSTALL) -D -m 0644 $(@D)/docker.service $(TARGET_DIR)/usr/lib/systemd/system
 
 	mkdir -p ${TARGET_DIR}/usr/local/lib/docker/cli-plugins/
 	$(INSTALL) -D -m 0755 $(@D)/docker-compose-linux-aarch64 ${TARGET_DIR}/usr/local/lib/docker/cli-plugins/docker-compose
--- scripts/post-fakeroot.sh
+++ scripts/post-fakeroot.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "  ✓ This script runs before creating filesystem images"
+
+
+echo "    - Remove docker.service on boot"
+rm -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/docker.service
+echo ""
+
+echo "    - Remove mail check"
+sed -i 's/^MAIL_CHECK_ENAB.*/MAIL_CHECK_ENAB\t\tno/' ${TARGET_DIR}/etc/login.defs
+echo ""
