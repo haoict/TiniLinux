dc905a123b98b6995a7ab1871fd9efe62d69c521
Hao Nguyen <hao.ict56@gmail.com>
2026-04-20 18:31:05 +0900

    update pc_qemu_aarch64_virt

--- scripts/rootfs-custom-scripts/4-post-image.sh
+++ scripts/rootfs-custom-scripts/4-post-image.sh
@@ -2,5 +2,7 @@
 
 echo "  ✓ This script runs after creating filesystem images"
 
-echo "    - Rename rootfs.squashfs to root.img"
-mv ${BINARIES_DIR}/rootfs.squashfs ${BINARIES_DIR}/root.img > /dev/null
+if [ -f "${BINARIES_DIR}/rootfs.squashfs" ]; then
+    echo "    - Rename rootfs.squashfs to root.img"
+    mv ${BINARIES_DIR}/rootfs.squashfs ${BINARIES_DIR}/root.img > /dev/null
+fi
