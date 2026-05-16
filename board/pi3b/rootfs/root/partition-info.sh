137f8953bc16418b69ac8eb0b597824fc62c0d77
Hao Nguyen <hao.ict56@gmail.com>
2026-05-16 09:00:21 +0900

    add some dev packages to pi3b

--- board/pi3b/rootfs/root/partition-info.sh
+++ board/pi3b/rootfs/root/partition-info.sh
@@ -1,4 +1,4 @@
-BOOT_SIZE=160 # minimum F32 size is 32MiB
+BOOT_SIZE=250 # minimum F32 size is 32MiB
 ROOTFS_INIT_SIZE=50 # the initial rootfs size to make flashable image (MiB)
-ROOTFS_TO_EXTEND_SIZE=1024 # the extra size to extend rootfs on first boot (MiB)
+ROOTFS_TO_EXTEND_SIZE=4096 # the extra size to extend rootfs on first boot (MiB)
 BOOT_PART_START=1 # default by buildroot raspberry config (in sectors)
