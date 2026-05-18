6f9023cb5f06d6c32f2505ba354cda3fe9194560
Hao Nguyen <hao.ict56@gmail.com>
2026-05-18 23:53:33 +0900

    add development packages to pi3b

--- board/pi3b/rootfs/root/firstboot.sh
+++ board/pi3b/rootfs/root/firstboot.sh
@@ -8,6 +8,9 @@ mv /root/firstboot.sh /root/.firstboot-done.sh
 # Persist /var
 systemctl mask var.mount
 
+# fsck /boot
+fsck.fat -a $(cat /proc/cmdline | sed -n 's/.*bootpart=\([^ ]*\).*/\1/p')
+
 echo "Rebooting..."
 sleep 1
 reboot
